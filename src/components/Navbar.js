35d1aa0ad3fcbe4ee095be855644d7d8400ab4ef
artem.zagoveliev <mexell96@gmail.com>
2021-07-09 13:42:04 +0300

    changed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import HistoryIcon from "@material-ui/icons/History";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     position: "fixed",
@@ -15,34 +15,41 @@ const useStyles = makeStyles({
     backgroundColor: "#2d313a",
     zIndex: 100,
   },
-});
+  button: {
+    margin: theme.spacing(0),
+    color: "white",
+  },
+}));
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
 
   return (
     <BottomNavigation showLabels className={classes.root}>
-      <Link to="/">
-        <BottomNavigationAction
-          style={{ color: "white" }}
-          label="Home"
-          icon={<HomeIcon />}
-        />
-      </Link>
-      <Link to="/movies">
-        <BottomNavigationAction
-          style={{ color: "white" }}
-          label="Movies"
-          icon={<SearchIcon />}
-        />
-      </Link>
-      <Link to="/previous-searches">
-        <BottomNavigationAction
-          style={{ color: "white" }}
-          label="Previous search"
-          icon={<HistoryIcon />}
-        />
-      </Link>
+      <Button
+        color="primary"
+        component={Link}
+        to="/"
+        startIcon={<HomeIcon />}
+        className={classes.button}>
+        Home
+      </Button>
+      <Button
+        color="primary"
+        component={Link}
+        to="/movies"
+        startIcon={<SearchIcon />}
+        className={classes.button}>
+        Movies
+      </Button>
+      <Button
+        color="primary"
+        component={Link}
+        to="/previous-searches"
+        startIcon={<HistoryIcon />}
+        className={classes.button}>
+        Previous search
+      </Button>
     </BottomNavigation>
   );
 }
