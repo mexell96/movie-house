4213456de03f16c32ba582aa88c68ad75e3a802b
artem.zagoveliev <mexell96@gmail.com>
2021-07-12 12:26:59 +0300

    created page previousSearches

--- src/pages/PreviousSearches/PreviousSearches.js
+++ src/pages/PreviousSearches/PreviousSearches.js
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import "./PreviousSearches.css";
+
 const PreviousSearches = () => {
+  const previousSearches = useSelector((state) => state.previousSearches);
+
   return (
     <>
-      <h1>PreviousSearches</h1>
+      <h1>PREVIOUS SEARCHES</h1>
+      <div className="previousSearches">
+        {previousSearches &&
+          previousSearches.map((request) => {
+            const body = (
+              <Link
+                to={`/movies?s=${request.input}&page=${request.page}`}
+                key={request.key}
+                className="request"
+                onClick={() => window.scroll(0, 0)}>
+                <div className="information">
+                  Looked for <span>{request.input}</span> on page{" "}
+                  <span>{request.page}</span>
+                </div>
+              </Link>
+            );
+            return request.input && request.page && body;
+          })}
+      </div>
     </>
   );
 };
