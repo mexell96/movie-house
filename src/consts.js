f3a5a89e17d97e5f1fcabee8f2cda0d000f17a87
artem.zagoveliev <mexell96@gmail.com>
2021-07-14 15:40:13 +0300

    changed protocol

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://www.omdbapi.com";
+export const API_URL = "https://www.omdbapi.com";
 export const API_KEY = "21f3dae8";
 export const noPicture =
   "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
