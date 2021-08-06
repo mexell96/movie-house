945b0a826effa324844734eedfff6d7e71b626eb
artem.zagoveliev <mexell96@gmail.com>
2021-08-06 12:46:42 +0300

    added auth in header

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-export const HeaderStyled = styled.span`
+export const HeaderStyled = styled.div`
   width: 100%;
   cursor: pointer;
   position: fixed;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   text-transform: uppercase;
   background-color: #39445a;
   font-family: "Montserrat", sans-serif;
   font-size: 5vw;
-  padding-bottom: 15px;
+  padding: 10px;
   box-shadow: 0px 1px 5px black;
   color: white;
   z-index: 100;
@@ -19,4 +19,16 @@ export const HeaderStyled = styled.span`
     padding-top: 15px;
     font-size: 6.4vw;
   }
+`;
+
+export const HeaderAvatarStyled = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderImgStyled = styled.img`
+  height: 5vw;
+  width: 5vw;
+  border-radius: 50%;
+  margin-right: 10px;
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,32 @@
-import { HeaderStyled } from "./Header.styled";
+import {
+  HeaderStyled,
+  HeaderAvatarStyled,
+  HeaderImgStyled,
+} from "./Header.styled";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { noPicture } from "../../consts";
 
 const Header = () => {
   return (
-    <HeaderStyled onClick={() => window.scroll(0, 0)}>Movie-house</HeaderStyled>
+    <HeaderStyled onClick={() => window.scroll(0, 0)}>
+      <div>Movie-house</div>
+      <HeaderAvatarStyled>
+        {/* {isAuth && (
+          <Button>
+            <Link to={`/auth`}>Login</Link>
+          </Button>
+        )} */}
+        {/* {!isAuth && (
+          <>
+            <HeaderImgStyled src={noPicture} alt="avatar" />
+            <Button>
+              <Link to={`/auth`}>Logout</Link>
+            </Button>
+          </>
+        )} */}
+      </HeaderAvatarStyled>
+    </HeaderStyled>
   );
 };
 
