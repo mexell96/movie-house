63aa5afec58f79d7098120c3d0620035e1ce85ce
artem.zagoveliev <mexell96@gmail.com>
2021-08-11 17:40:51 +0300

    changed FormImage

--- src/components/FormImage/FormImage.style.ts
+++ src/components/FormImage/FormImage.style.ts
@@ -1,7 +1,29 @@
 import styled from "styled-components";
+import { Button } from "@material-ui/core";
 
-export const FormImageStyled = styled.img`
-  width: 200px;
-  height: 200px;
+type FormImageButtonType = {
+  component: string;
+};
+
+export const FormImageButtonStyled = styled(Button)<FormImageButtonType>`
+  border-radius: 50% !important;
+  padding: 0 !important;
+`;
+
+export const FormImageErrorStyled = styled.div`
+  color: #f44336;
+  margin: 0;
+  font-size: 0.75rem;
+  margin-top: 3px;
+  text-align: left;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-weight: 400;
+  line-height: 1.66;
+  letter-spacing: 0.03333em;
+`;
+
+export const FormImageAvatarStyled = styled.img`
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
 `;
--- src/components/FormImage/FormImage.tsx
+++ src/components/FormImage/FormImage.tsx
@@ -1,13 +1,97 @@
-import { FormImageStyled } from "./FormImage.style";
+import { useState, useEffect } from "react";
+import { Button, message } from "antd";
 
-import { noPicture } from "../../consts";
+import {
+  FormImageButtonStyled,
+  FormImageErrorStyled,
+  FormImageAvatarStyled,
+} from "./FormImage.style";
+
+import { noPicture, MEGABYTE } from "../../consts";
+import { useHttp } from "../../hooks/http.hook";
 
 type FormImagePropType = {
   avatar: string;
+  id: string;
+  token: string;
+  getUser: () => Promise<void>;
 };
 
-const FormImage = ({ avatar }: FormImagePropType) => {
-  return <FormImageStyled src={avatar || noPicture} alt="avatar" />;
+const FormImage = ({ avatar, id, token, getUser }: FormImagePropType) => {
+  const [sizeError, setSizeError] = useState(false);
+  const [editAvatar, setEditAvatar] = useState(false);
+  const [logo, setLogo] = useState("");
+  const { loading, error, request, clearError } = useHttp();
+
+  const onFileChange = (input: any) => {
+    const file = input.target.files[0];
+    if (file && file.size < MEGABYTE) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log("avatar", reader.result as string);
+        setEditAvatar(true);
+        setLogo(reader.result as string);
+      };
+      reader.onerror = () => console.log(reader.error, "error");
+    } else {
+      setSizeError(true);
+    }
+  };
+
+  useEffect(() => {
+    if (error !== null) {
+      message.error(`${error}`);
+      clearError();
+    }
+  }, [error, clearError]);
+
+  const changeAvatar = ({ logo }: any) => {
+    (async () => {
+      try {
+        const response = await request(
+          `/api/profile-name/${id}`,
+          "PATCH",
+          { logo },
+          { Authorization: `Bearer ${token}` }
+        );
+        message.success(response.message);
+        setEditAvatar(false);
+        getUser();
+      } catch (e) {
+        console.log(e, "E message createUserPage");
+      }
+    })();
+  };
+
+  return (
+    <div>
+      <FormImageButtonStyled component="label">
+        <FormImageAvatarStyled src={logo || noPicture} alt="avatar" />
+        <input
+          accept="image/*"
+          id="avatar"
+          name="avatar"
+          type="file"
+          hidden
+          onChange={onFileChange}
+        />
+      </FormImageButtonStyled>
+      {sizeError && (
+        <FormImageErrorStyled>
+          Large file size choose another one
+        </FormImageErrorStyled>
+      )}
+      {editAvatar && (
+        <Button onClick={() => changeAvatar(logo)} disabled={loading}>
+          Save
+        </Button>
+      )}
+      {editAvatar && (
+        <Button onClick={() => setEditAvatar(false)}>Cancel</Button>
+      )}
+    </div>
+  );
 };
 
 export { FormImage };
