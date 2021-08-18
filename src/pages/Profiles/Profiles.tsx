0f3d8e8dd3317c2c839f47ddd7f4501719ce6c43
artem.zagoveliev <mexell96@gmail.com>
2021-08-18 14:35:39 +0300

    added change role

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,55 +1,98 @@
 import { useCallback, useContext, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Table, Select, message } from "antd";
 
-import { ProfileImgStyled, ProfileWrapperStyled } from "./Profiles.style";
+import { ProfileErrorStyled } from "./Profiles.style";
 
 import { useHttp } from "../../hooks/http.hook";
 import { AuthContext } from "../../context/AuthContext";
 import { Loader } from "../../components";
-import { noPicture } from "../../consts";
 
-const body = (data: any) => {
-  if (data.length > 0) {
-    return (
-      <>
-        {data.map((user: any) => (
-          <ProfileWrapperStyled key={user._id}>
-            <h2>Profile</h2>
-            <div>{user._id}</div>
-            <div>{user.name}</div>
-            <div>{user.email}</div>
-            <ProfileImgStyled src={user.avatar || noPicture} alt="avatar" />
-          </ProfileWrapperStyled>
-        ))}
-      </>
-    );
-  }
-};
+const { Option } = Select;
 
 const Profiles = () => {
   const { token } = useContext(AuthContext);
   const { request, loading } = useHttp();
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState<UserType[] | []>([]);
+  const userReducer = useSelector(
+    ({ userReducer }: RootStateType) => userReducer
+  );
 
-  const getName = useCallback(async () => {
+  const getUsers = useCallback(async () => {
     try {
       const users = await request(`/api/profiles`, "GET", null, {
         Authorization: `Bearer ${token}`,
       });
-      setData(users);
+      setUsers(users);
     } catch (e) {
       console.log(e, "Error profiles");
     }
   }, [token, request]);
 
+  const changeRole = (role: string, id: string) => {
+    (async () => {
+      try {
+        const response = await request(
+          `/api/profile-role/${id}`,
+          "PATCH",
+          { role },
+          { Authorization: `Bearer ${token}` }
+        );
+        message.success(response.message);
+        getUsers();
+      } catch (e) {
+        console.log(e, "E message createUserPage");
+      }
+    })();
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+      render: (role: string, user: any) => {
+        return (
+          <Select
+            value={role}
+            style={{ width: 120 }}
+            onChange={(value) => changeRole(value, user._id)}>
+            <Option value="ADMIN">ADMIN</Option>
+            <Option value="USER">USER</Option>
+          </Select>
+        );
+      },
+    },
+  ];
+
   useEffect(() => {
-    getName();
-  }, [getName]);
+    userReducer.role === "ADMIN" && getUsers();
+  }, [getUsers, userReducer]);
 
   if (loading) {
     return <Loader />;
   }
 
-  return <>{!loading && body(data)}</>;
+  return (
+    <>
+      {!loading && users.length !== 0 && (
+        <Table bordered columns={columns} dataSource={users} rowKey="_id" />
+      )}
+      {!loading && userReducer.role !== "ADMIN" && (
+        <ProfileErrorStyled>You don't have access.</ProfileErrorStyled>
+      )}
+    </>
+  );
 };
 
 export { Profiles };
