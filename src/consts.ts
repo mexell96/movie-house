65b116be806477375a4ff08123cbe64a28675c6c
artem.zagoveliev <mexell96@gmail.com>
2021-09-01 16:52:08 +0300

    changed consts

--- src/consts.ts
+++ src/consts.ts
@@ -6,5 +6,5 @@ export const MOVIES_NUMBER_ON_ONE_PAGE: number = 10;
 export const PORTION_OF_PAGES: number = 3;
 export const stars = [1, 2, 3, 4, 5];
 export const MEGABYTE: number = 1048576;
-export const API_BASE = "https://movie-house-back.herokuapp.com";
-// export const API_BASE = "http://localhost:5000";
+export const API_BASE = "https://movie-house-back.herokuapp.com/api";
+// export const API_BASE = "http://localhost:5000/api";
